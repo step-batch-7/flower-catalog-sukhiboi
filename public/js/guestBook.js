b11255fd4fa8c2d828732e83e9c47f0914b099fd
sukhiboi <41sukhdevsingh@gmail.com>
2020-01-30 11:13:34 +0530

    Updated queryParser
    Now using node's querystring to parse the query text.
    
    Deleted queryParser.js -> because using node's parser
    
    Deleted guestBook.js -> That file was created to parse the query string
    at fronted but now that problem is solved by querystring in backend.

--- public/js/guestBook.js
+++
@@ -1,6 +0,0 @@
-const formatCommentMessages = function() {
-  const comments = document.querySelectorAll('.comment p');
-  comments.forEach(comment => {
-    comment.innerText = decodeURIComponent(comment.innerText);
-  });
-};
