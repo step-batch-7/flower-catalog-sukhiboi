d24316e1abb32d2e779f22a03ceddeffe9fb6b87
sukhiboi <41sukhdevsingh@gmail.com>
2020-01-30 07:25:32 +0530

    Added router
    Added router for different routes and listed them in server.js
    Removed handle request, because its job is done by router and the routes.
    Created router.js for Router.

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const http = require('http');
-const { handleRequest, dataStorePath } = require('./lib/responseHandler');
 const { writeFileSync, existsSync } = require('fs');
-
-const server = http.Server(handleRequest);
-server.listen(process.argv[2] || 8000);
+const {
+  err404,
+  serveTemplate,
+  guestBookHandler,
+  serveStaticPage,
+  dataStorePath
+} = require('./lib/responseHandler');
+const { Router } = require('./lib/router');
 
 const dataStoreExists = existsSync(dataStorePath);
 if (!dataStoreExists) writeFileSync(dataStorePath, '[]');
+
+const router = new Router();
+
+router.get('', serveStaticPage);
+router.get('/', serveStaticPage);
+router.get('/guest-book.html', serveTemplate);
+router.post('/guest-book.html', guestBookHandler);
+
+router.get('', err404);
+router.post('', err404);
+
+const ROUTER = router.serve.bind(router);
+
+const server = http.Server(ROUTER);
+server.listen(process.argv[2] || 8000);
