9848146708874e6e86dff17aa62dafe492a581fc
sukhiboi <41sukhdevsingh@gmail.com>
2020-01-31 11:01:53 +0530

    Created app.js
    Moved all the router reltaed functionality to app.js, to make it more
    readable.

--- server.js
+++ server.js
@@ -1,31 +1,10 @@
 const http = require('http');
 const { fileExists, saveContent } = require('./lib/fileOperations');
-const {
-  err404,
-  serveTemplate,
-  guestBookHandler,
-  serveStaticPage
-} = require('./lib/handlers');
-const { bodyParser } = require('./lib/middlewares');
 const { dataStorePath, PORT } = require('./config.js');
-const { Router } = require('./lib/router');
+const { app } = require('./lib/app');
 
 const dataStoreExists = fileExists(dataStorePath);
 if (!dataStoreExists) saveContent(dataStorePath, '[]');
 
-const router = new Router();
-
-router.use(bodyParser);
-
-router.get('', serveStaticPage);
-router.get('/', serveStaticPage);
-router.get('/guest-book.html', serveTemplate);
-router.post('/guest-book.html', guestBookHandler);
-
-router.get('', err404);
-router.post('', err404);
-
-const ROUTER = router.serve.bind(router);
-
-const server = http.Server(ROUTER);
+const server = http.Server(app);
 server.listen(PORT);
