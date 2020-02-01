b9cf05f0715c263471764ffe96158738c5e0481f
sukhiboi <41sukhdevsingh@gmail.com>
2020-02-01 08:28:59 +0530

    Added test for flower pages
    Added test for abeliophyllum and ageratum flower page

--- test/testHandlers.js
+++ test/testHandlers.js
@@ -1,14 +1,38 @@
 const request = require('supertest');
 const { app } = require('./../lib/app');
 
-describe('#GET /()', () => {
-  it('should response back with index page', (done) => {
+describe('#GET /', () => {
+  it('should response back with index page', done => {
     request(app)
       .get('/')
       .set('Accept', 'text/html, text/css, application/js')
-      .expect(200, done)
+      .expect(200)
       .expect('Content-Type', 'text/html')
       .expect('Content-Length', '1221')
-      .expect(/hideWaterPot/);
+      .expect(/hideWaterPot/, done);
+  });
+});
+
+describe('#GET /abeliophyllum', () => {
+  it('should response back with abeliophyllum page', done => {
+    request(app)
+      .get('/abeliophyllum.html')
+      .set('Accept', 'text/html, text/css')
+      .expect(200)
+      .expect('Content-Type', 'text/html')
+      .expect('Content-Length', '1795')
+      .expect(/distichum/, done);
+  });
+});
+
+describe('#GET /ageratum', () => {
+  it('should response back with abeliophyllum page', done => {
+    request(app)
+      .get('/ageratum.html')
+      .set('Accept', 'text/html, text/css')
+      .expect(200)
+      .expect('Content-Type', 'text/html')
+      .expect('Content-Length', '1574')
+      .expect(/perennials/, done);
   });
 });
