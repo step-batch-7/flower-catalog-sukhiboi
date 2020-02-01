28e8d143ffdab78bc452ab9ef4abf49b715d2603
sukhiboi <41sukhdevsingh@gmail.com>
2020-02-01 12:32:29 +0530

    Added script to start server
    Added script to start server i.e npm start and provided it a exported a
    dataStorePath in shell.
    
    Updated config.js -> Now it take dataStorePath directy from shell's environment

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const dataStorePath = process.env.DATASTORE || `${__dirname}/DATASTORE.json`;
+const dataStorePath = process.env.DATASTORE;
 const [, , customPort] = process.argv;
 const defaultPORT = 8000;
 const PORT = customPort || defaultPORT;
